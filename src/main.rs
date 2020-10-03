c0503bb32b19a2fc9e7765fff49115114a8e83bd
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-10-03 22:51:58 +1000

    Read keypresses and send them to the server

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,56 @@
-use polyp::ServerMsg;
-use std::io::Read;
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyModifiers};
+use crossterm::terminal;
+use polyp::{ClientMsg, ServerMsg};
+use std::io::{BufRead, BufReader, Write};
 use std::process::{Command, Stdio};
 
+const CTRL_C_EVENT: Event = Event::Key(KeyEvent {
+    code: KeyCode::Char('c'),
+    modifiers: KeyModifiers::CONTROL,
+});
+
 fn main() -> anyhow::Result<()> {
-    let server = Command::new("polyp-server")
+    terminal::enable_raw_mode()?;
+
+    let mut server = Command::new("polyp-server")
+        .stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .spawn()?;
 
-    let mut server_stdout = server.stdout.unwrap();
-    let mut server_output = Vec::new();
-    server_stdout.read_to_end(&mut server_output)?;
+    let mut server_stdin = server.stdin.as_mut().unwrap();
+    let mut server_stdout = BufReader::new(server.stdout.as_mut().unwrap());
 
-    let server_msg: ServerMsg = serde_json::from_slice(&server_output)?;
+    loop {
+        match event::read()? {
+            CTRL_C_EVENT => {
+                eprintln!("polyp-cli-client: shutting down...\r");
+                server.kill()?;
+                terminal::disable_raw_mode()?;
 
-    match server_msg {
-        ServerMsg::NewText(text) => println!("{}", text),
+                return Ok(());
+            }
+            Event::Key(KeyEvent {
+                code: KeyCode::Char(c),
+                ..
+            }) => {
+                serde_json::to_writer(&mut server_stdin, &ClientMsg::PressedKey(c))?;
+                server_stdin.write_all(b"\n")?;
+                server_stdin.flush()?;
+
+                let server_msg = {
+                    let mut server_output = String::new();
+                    server_stdout.read_line(&mut server_output)?;
+                    serde_json::from_slice(server_output.as_bytes())?
+                };
+
+                match server_msg {
+                    ServerMsg::NewText(text) => {
+                        let text = text.replace('\n', "\r\n");
+                        println!("polyp-cli-client: got response from server:\r\n{}\r", text);
+                    }
+                }
+            }
+            _ => {}
+        }
     }
-
-    Ok(())
 }

b0a6cd257d57b65a7cb7174e1f40061c2ccbda3f
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-10-14 08:45:59 +1100

    Communicate using WebSockets

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyModifiers};
 use crossterm::terminal;
-use polyp::{ClientMsg, ServerMsg};
-use std::io::{BufRead, BufReader, Write};
-use std::process::{Command, Stdio};
+use polyp::{ServerMsg, UserInput};
+use std::net::{IpAddr, Ipv4Addr, SocketAddr, TcpListener};
+use std::process::Command;
+use tungstenite::Message;
 
 const CTRL_C_EVENT: Event = Event::Key(KeyEvent {
     code: KeyCode::Char('c'),
@@ -12,19 +13,29 @@ const CTRL_C_EVENT: Event = Event::Key(KeyEvent {
 fn main() -> anyhow::Result<()> {
     terminal::enable_raw_mode()?;
 
-    let mut server = Command::new("polyp-server")
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
+    let socket_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 1618);
+
+    Command::new("polyp-server")
+        .arg(socket_addr.to_string())
         .spawn()?;
 
-    let mut server_stdin = server.stdin.as_mut().unwrap();
-    let mut server_stdout = BufReader::new(server.stdout.as_mut().unwrap());
+    let mut server_websocket = {
+        let listener = TcpListener::bind(socket_addr)?;
+        println!("polyp-cli-client: listening at socket {:?}\r", socket_addr);
+
+        let stream = listener.incoming().next().unwrap()?;
+
+        tungstenite::server::accept(stream)?
+    };
+
+    println!("polyp-cli-client: connected to server\r");
 
     loop {
         match event::read()? {
             CTRL_C_EVENT => {
                 eprintln!("polyp-cli-client: shutting down...\r");
-                server.kill()?;
+
+                server_websocket.close(None)?;
                 terminal::disable_raw_mode()?;
 
                 return Ok(());
@@ -33,20 +44,28 @@ fn main() -> anyhow::Result<()> {
                 code: KeyCode::Char(c),
                 ..
             }) => {
-                serde_json::to_writer(&mut server_stdin, &ClientMsg::PressedKey(c))?;
-                server_stdin.write_all(b"\n")?;
-                server_stdin.flush()?;
+                println!("polyp-cli-client: received keystroke ‘{}’\r", c);
+
+                let serialized = serde_json::to_vec(&UserInput::PressedKey(c))?;
+                server_websocket.write_message(Message::Binary(serialized))?;
+
+                println!("polyp-cli-client: wrote user input to server\r");
 
                 let server_msg = {
-                    let mut server_output = String::new();
-                    server_stdout.read_line(&mut server_output)?;
-                    serde_json::from_slice(server_output.as_bytes())?
+                    let message = server_websocket.read_message()?;
+                    println!("polyp-cli-client: read message from server\r");
+
+                    if let Message::Binary(json) = message {
+                        serde_json::from_slice(&json)?
+                    } else {
+                        unreachable!();
+                    }
                 };
 
                 match server_msg {
                     ServerMsg::NewText(text) => {
                         let text = text.replace('\n', "\r\n");
-                        println!("polyp-cli-client: got response from server:\r\n{}\r", text);
+                        println!("polyp-cli-client: new text from server:\r\n{}\r", text);
                     }
                 }
             }

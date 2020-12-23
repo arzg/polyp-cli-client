dce335fecd981c9765e41b0905e1c2f6c7a534b1
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-12-23 15:51:20 +1100

    Refactor to remove handle_key function

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyModifiers};
 use crossterm::terminal;
 use polyp::protocol::Connection;
 use polyp::{Key, Ui, UserInput};
-use std::io::{self, BufRead, Write};
+use std::io::{self, Write};
 use std::process::{Command, Stdio};
 
 const CTRL_C_EVENT: Event = Event::Key(KeyEvent {
@@ -34,36 +34,29 @@ fn main() -> anyhow::Result<()> {
             _ => continue,
         };
 
-        match code {
-            KeyCode::Char(c) => handle_key(Key::Char(c), &mut server_connection)?,
-            KeyCode::Backspace => handle_key(Key::Backspace, &mut server_connection)?,
-            KeyCode::Up => handle_key(Key::Up, &mut server_connection)?,
-            KeyCode::Down => handle_key(Key::Down, &mut server_connection)?,
-            KeyCode::Left => handle_key(Key::Left, &mut server_connection)?,
-            KeyCode::Right => handle_key(Key::Right, &mut server_connection)?,
-            _ => {}
-        }
+        let key = match code {
+            KeyCode::Char(c) => Key::Char(c),
+            KeyCode::Backspace => Key::Backspace,
+            KeyCode::Up => Key::Up,
+            KeyCode::Down => Key::Down,
+            KeyCode::Left => Key::Left,
+            KeyCode::Right => Key::Right,
+            _ => continue,
+        };
+
+        eprintln!("polyp-cli-client: received keystroke ‘{:?}’\r", key);
+
+        server_connection.send_message(&UserInput::PressedKey(key))?;
+        eprintln!("polyp-cli-client: wrote user input to server\r");
+
+        let ui = server_connection.recv_message()?;
+        eprintln!("polyp-cli-client: read UI from server\r");
+
+        let mut stdout = io::stdout();
+        crossterm::queue!(stdout, terminal::Clear(terminal::ClearType::CurrentLine))?;
+        print!("\r{}", format_ui(ui));
+        stdout.flush()?;
     }
-}
-
-fn handle_key(
-    key: Key,
-    server_connection: &mut Connection<impl BufRead, impl Write>,
-) -> anyhow::Result<()> {
-    eprintln!("polyp-cli-client: received keystroke ‘{:?}’\r", key);
-
-    server_connection.send_message(&UserInput::PressedKey(key))?;
-    eprintln!("polyp-cli-client: wrote user input to server\r");
-
-    let ui = server_connection.recv_message()?;
-    eprintln!("polyp-cli-client: read message from server\r");
-
-    let mut stdout = io::stdout();
-    crossterm::queue!(stdout, terminal::Clear(terminal::ClearType::CurrentLine))?;
-    print!("\r{}", format_ui(ui));
-    stdout.flush()?;
-
-    Ok(())
 }
 
 fn format_ui(ui: Ui) -> String {
